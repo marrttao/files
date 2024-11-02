b41aa58923d173f655557cc30126ae83a7787dc6
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-02 23:19:58 +0200

    boosted speed of my code and logic problems

--- main.py
+++ main.py
@@ -7,11 +7,6 @@ if __name__ == "__main__":
 
     with open("text_file.txt", "w") as file:
         for line in lines:
-            words = line.split()
-            new_words = []
-            for word in words:
-                if word_to_replace in word:
-                    word = word.replace(word_to_replace, replacing_word)
-                new_words.append(word)
-            new_line = ' '.join(new_words)
-            file.write(new_line + '\n')
+            if word_to_replace in line:
+                line = line.replace(word_to_replace, replacing_word)
+            file.write(line)
