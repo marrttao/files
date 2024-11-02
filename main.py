da10397beaf6a776f57b380a6afe3b61071496b8
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-02 20:12:48 +0200

    completed task 6

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import string
+def issymbol(char): #сделал функцию что бы скоротить код (мог бы сделать паттерном либо словарем)
+    return char in string.punctuation
 if __name__ == "__main__":
-    pass
+    with open("text_file.txt", "r") as file:
+        lines = file.readlines()
+        count = 0
+        for line in lines:
+            for char in line:
+                if issymbol(char):
+                    count += 1
+        print(count)
