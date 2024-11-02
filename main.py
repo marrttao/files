cf1168e8d07e67c138fa44ba3eb5662c01a37165
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-02 20:17:01 +0200

    completed task 7

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 if __name__ == "__main__":
-    lines = ["That cool", "idk why", "but its cool", "dumb text xD", "And again, thats cool"]
+    lines = ["That cool!", "idk why", "but its cool", "dumb text xD", "And again, thats cool"]
     with open("text_file.txt", "w") as file:
         for line in lines:
             file.write(line + "\n")
