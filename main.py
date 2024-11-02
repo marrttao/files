41b0bf1a2a10e6c85e264c40d06495b6b0cd5ca0
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-02 23:29:39 +0200

    boosted speed of my code and logic problems

--- main.py
+++ main.py
@@ -3,13 +3,9 @@ if __name__ == "__main__":
         lines = file.readlines()
     with open("new_text_file.txt", "w") as new_file:
         for line in lines:
-            words = line.split()
-            new_words = []
-            for word in words:
-                if '*' in word:
-                    word = word.replace('*', '&')
-                elif '&' in word:
-                    word = word.replace('&', '*')
-                new_words.append(word)
-            new_line = ' '.join(new_words)
-            new_file.write(new_line + '\n')
+            if '*' in line:
+                line = line.replace('*', 'TEMP_PLACEHOLDER')
+            if '&' in line:
+                line = line.replace('&', '*')
+            line = line.replace('TEMP_PLACEHOLDER', '&')  #без плейсхолдера почему-то не работает + прочитал что это
+            new_file.write(line)
