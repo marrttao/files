1cab62af291574ffa1d11b34bb6fa63dbfae6143
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-02 17:05:37 +0200

    completed task 1

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 if __name__ == "__main__":
-    pass
+    with open("text_file.txt", "r") as file:
+        lines = file.readlines()
+
+    with open("new_text_file.txt", "w") as new_file:
+        for line in lines:
+            words = line.split()
+            for word in words:
+                if len(word) >= 7:
+                    new_file.write(word + "\n")
