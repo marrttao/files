9efb81eac6a99414fb220c829df301adcd978af4
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-02 17:29:31 +0200

    completed task 4

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 if __name__ == "__main__":
-    pass
+    with open("text_file.txt", "r") as file:
+        lines = file.readlines()
+    for i, line in enumerate(lines):
+        if ',' not in line:
+            lines[i] = line + "************\n"
+    with open("new_text_file.txt", "w") as newfile:
+        newfile.writelines(lines)
