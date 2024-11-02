330fa80233f0f91d65cfc1342300827345ade984
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-02 21:46:56 +0200

    completed task 1

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 if __name__ == "__main__":
-    pass
+    with open("first_text_file.txt", "r") as file:
+        lines_one = file.readlines()
+    with open("second_text_file.txt", "r") as file:
+        lines_two = file.readlines()
+    printed_lines = set()
+    for line_one in lines_one:
+        for line_two in lines_two:
+            #тут скажете что лишние строки но без них результат дублировался (без проверки есть ли строка в списке строк которые мы записали)
+            #тут скажете что лишние строки но без них результат дублировался (без проверки есть ли строка в списке строк которые мы записали)
+            #тут скажете что лишние строки но без них результат дублировался (без проверки есть ли строка в списке строк которые мы записали)
+            if line_one != line_two:
+                if line_one not in printed_lines:
+                    print(line_one)
+                    printed_lines.add(line_one)
+                if line_two not in printed_lines:
+                    print(line_two)
+                    printed_lines.add(line_two)
