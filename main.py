8e88c6756a1680ed213c10c2802d25e8bb4cc75e
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-02 23:01:13 +0200

    completed task 4

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 if __name__ == "__main__":
     with open("text_file.txt", "r") as file:
-        pass
+        lines = file.readlines()
+
+    word_to_replace = input("Enter the word to replace: ")
+    replacing_word = input("Enter the replacing word: ")
+
+    with open("text_file.txt", "w") as file:
+        for line in lines:
+            words = line.split()
+            new_words = []
+            for word in words:
+                if word_to_replace in word:
+                    word = word.replace(word_to_replace, replacing_word)
+                new_words.append(word)
+            new_line = ' '.join(new_words)
+            file.write(new_line + '\n')
