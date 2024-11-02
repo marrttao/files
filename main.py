6be4c4dfc43184f0d6a3cb5065202f597c778879
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-02 17:47:16 +0200

    completed task 6

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 if __name__ == "__main__":
-    pass
+    with open("text_file.txt", "r") as file:
+        lines = file.readlines()
+    with open("new_text_file.txt", "w") as new_file:
+        for line in lines:
+            words = line.split()
+            new_words = []
+            for word in words:
+                if '*' in word:
+                    word = word.replace('*', '&')
+                elif '&' in word:
+                    word = word.replace('&', '*')
+                new_words.append(word)
+            new_line = ' '.join(new_words)
+            new_file.write(new_line + '\n')
