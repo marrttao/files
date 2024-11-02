5c470ed846402e7debfb6b450d8aa4b6da64c87a
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-02 23:18:14 +0200

    completed task 5

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 if __name__ == "__main__":
     with open("text_file.txt", "r") as file:
-        pass
+        lines = file.readlines()
+        word = input("Word ")
+        word_count = 0
+        for line in lines:
+            words = line.split()
+            word_count += words.count(word)
+        print(f"Word {word} was found {word_count} times.")
