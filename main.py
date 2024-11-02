7feb7de2571e473903bb33333cc6c0c293dac1a4
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-02 22:21:59 +0200

    completed task 3

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 if __name__ == "__main__":
     with open("text_file.txt", "r") as file:
-        pass
+        lines = file.readlines()
+    if lines:
+        del lines[-1]
+    with open("new_text_file.txt", "w") as newfile:
+        newfile.writelines(lines)
