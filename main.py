9e37e0fd28919eb5c12a0f7858cec73f4ac31fc5
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-02 17:15:21 +0200

    completed task 3

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 if __name__ == "__main__":
-    pass
+    with open("text_file.txt", "r") as file:
+        lines = file.readlines()
+    with open("new_text_file.txt", "w") as new_file:
+        for line in lines[::-1]:
+            new_file.write(line + "\n")
