aee2193369ab8edd88d01d03c6eea193e1dfae69
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-02 23:10:52 +0200

    completed task 4

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 if __name__ == "__main__":
     with open("text_file.txt", "r") as file:
         lines = file.readlines()
-
-    word_to_replace = input("Enter the word to replace: ")
-    replacing_word = input("Enter the replacing word: ")
-
-    with open("text_file.txt", "w") as file:
+        longest_line = ""
         for line in lines:
-            words = line.split()
-            new_words = []
-            for word in words:
-                if word_to_replace in word:
-                    word = word.replace(word_to_replace, replacing_word)
-                new_words.append(word)
-            new_line = ' '.join(new_words)
-            file.write(new_line + '\n')
+            if len(line) > len(longest_line):
+                longest_line = line
+        print(longest_line)
