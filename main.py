20e5f472bb7257c397607233fb892c5e04a520ed
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-02 17:34:45 +0200

    completed task 5

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 if __name__ == "__main__":
-    pass
+    symbol = input("Enter a symbol: ")
+    count = 0
+    with open("text_file.txt", "r") as file:
+        lines = file.readlines()
+
+    for line in lines:
+        words = line.split()
+        for word in words:
+            if symbol in word:
+                count += 1
+    print(f"Symbol '{symbol}' was found {count} times ")
